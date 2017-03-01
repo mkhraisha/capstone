785a3f8af959f08abf08e1a628ea0bc569a5b59b
mkhraisha <mkhraisha@hotmail.com>
2017-02-28 20:39:20 -0500

    Added scripts

---
+++ Scripts/Models/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Player {
+
+	/*
+	public struct TilePosition {
+		public int x;
+		public int y;
+	}
+	TilePosition selectedTile;
+	public TilePosition SelectedTile {
+		get { return selectedTile; }
+		set { selectedTile = value; }
+	}
+	*/
+
+	public int selectedX;
+	public int selectedY;
+
+	public Player(short playerNumber) {
+		if (playerNumber == 1) {
+			this.selectedX = 0;
+			this.selectedY = 0;
+		} else if (playerNumber == 2) {
+			this.selectedX = 9;
+			this.selectedY = 9;
+		}
+	}
+}
